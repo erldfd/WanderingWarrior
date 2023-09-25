4f297d14411518d547ce5c4eebb1771dc00f6b17
erldfd <hseongrae@gmail.com>
2023-09-26 03:34:54 +0900

    마법사 캐릭터 클래스 생성

---
+++ Source/WanderingWarrior/Character/WizardCharacter.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Character/WWCharacter.h"
+
+#include "WizardCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WANDERINGWARRIOR_API AWizardCharacter : public AWWCharacter
+{
+	GENERATED_BODY()
+	
+public: 
+
+	AWizardCharacter();
+};
